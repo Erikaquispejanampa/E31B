8444ebf95e2aacaad5a5db9208f78d90e4fb6a45
BBQ <erikaquispejanampa@gmail.com>
2021-11-06 08:28:14 -0500

    Redirección de ruta vacía

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'aboutus', component: AboutusComponent },
   { path: 'contactus', component: ContactusComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
